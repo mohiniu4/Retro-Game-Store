05960480bb5141aa4e2874a28ef2fcef404c2d49
mohiniu4 <110068413+mohiniu4@users.noreply.github.com>
2025-06-12 15:30:57 -0400

    Create Program.cs

---
+++ GameStoreData/Program.cs
@@ -0,0 +1,11 @@
+//this Program.cs exists just to keep Visual Studio happy.
+//GameStoreData is a class library, not meant to be run directly
+
+namespace GameStoreData
+{
+    public static class Program
+    {
+        //this does absolutely nothing, just a placeholder
+        public static void Main() { }
+    }
+}
